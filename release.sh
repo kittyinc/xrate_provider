7189717a458d7bf30156008fe56347a3cc1ea62c
Jordi Loyzaga <jordi@loyzaga.net>
2022-01-18 19:52:39 -0600

    Fixed release step that used previous heroku stack release step

--- release.sh
+++ release.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-pip install -r requirements.txt
 python xrate_provider/manage.py collectstatic --noinput 
 python xrate_provider/manage.py migrate
 echo "Post deploy done, restarting..."
